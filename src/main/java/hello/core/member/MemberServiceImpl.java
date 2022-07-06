f3f4ace5ee0b079b5d3200d83969fcc143657a5b
KJH <stonehead334@gmail.com>
2022-07-06 15:34:51 +0900

    관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,15 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    private final MemberRepository memberRepository;
+    // 추상화에만 의존한다
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    //구현체에 뭐가 들어갈지는 생성자를 통해서 전달한다
+
 
     @Override
     public void join(Member member) {

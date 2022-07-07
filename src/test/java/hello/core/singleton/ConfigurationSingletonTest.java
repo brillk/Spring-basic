d09a9d22783fe71df370c7225b4464a1cb6da4ea
KJH <stonehead334@gmail.com>
2022-07-07 14:18:04 +0900

    @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -31,4 +31,18 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+        // bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$6f837f94
+        // 순수한 자바 코드로는 class hello.core.AppConfig 까지만 나오지만
+        // @COnfiguration 안에 AppConfig을 복사하고 다른 코드를 추가한 저장소가 있다
+        // 그곳이 memberService를 처음 생성되고, 저장하고 다시 호출할때, 이곳에서 꺼내서 다시 쓴다
+        // 그래서 스프링 싱글톤을 가능케 한다
+    }
 }

cba0977947c632931ecaffcbaada5e12a3fa4ca8
KJH <stonehead334@gmail.com>
2022-07-07 13:00:51 +0900

    싱글톤 패턴

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,59 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    /*
+    싱글톤 해결법
+    객체가 딱 1개만 생성되고, 공유하도록 설계하면 된다
+
+    현재 유저가 요청을 할때마다 새로운 객체를 계속 만든다
+    문제점은 메모리 낭비와 속도 저하 이다.
+    장점:
+
+    데이터 공유가 쉽다는 것이다.
+    싱글톤 인스턴스가 전역으로 사용되는 인스턴스이기 때문에 다른 클래스의 인스턴스들이 접근하여 사용할 수 있다.
+    하지만 여러 클래스의 인스턴스에서 싱글톤 인스턴스의 데이터에
+    동시에 접근하게 되면 동시성 문제가 발생할 수 있으니 이점을 유의해서 설계하는 것이 좋다.
+    */
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        AppConfig appConfig = new AppConfig();
+
+        //1. 호출할때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 호출할때마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1); //MemberServiceImpl@66d18979
+        System.out.println("memberService2 = " + memberService2); //MemberServiceImpl@bccb269  서로 다른 객체 생성
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isEqualTo(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest( ){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1); //@63475ace
+        System.out.println("singletonService2 = " + singletonService2); //@63475ace
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+}

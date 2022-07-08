ca8183a73fb30b6261a623a78cdcbecef699ce55
KJH <stonehead334@gmail.com>
2022-07-08 15:24:05 +0900

    조회 빈의 2개 이상 - 문제

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -4,7 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
-
+@Component //일부러 조회하는 빈이 2개 이상일떄의 오류를 나타냈다
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 할인 금액
